d87bb0a9ad5af1788299bd61251019048cf21e7b
Admin BasketGate <admin@basketgate.io>
2019-11-25 23:36:37 +0200

    fix error with alert

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,6 @@
 var number = "";
+var user_name = "";
+var user_email = "";
 (function ($) {
 	"use strict";
 
@@ -7,7 +9,7 @@ var number = "";
 	var input = $('.validate-input .input100');
 
 	$('.validate-form').on('submit', function () {
-	console.log("Index forms ...");
+	    console.log("Index forms ..."+ input.length);
 		var check = true;
 
 		for (var i = 0; i < input.length; i++) {
@@ -28,9 +30,10 @@ var number = "";
 	});
 
 	function validate(input) {
-	var att=$(input).attr('name')
-	console.log("number : " + att);
-		if ($(input).attr('name') == 'mobile') {
+	    var att=$(input).attr('name')
+
+		if (att == 'mobile') {
+		    console.log("number : " + att);
 			number = $(input).val();
 
 			if (number.length == 10 && number != null) {
@@ -61,7 +64,7 @@ var number = "";
 			return false;
 		}
     else
-		if ($(input).attr('name') == 'pin') {
+		if (att == 'pin') {
 			var otp = $(input).val();
 
 			if (otp.length == 4 && otp != null && otp == '7531') {
@@ -73,18 +76,59 @@ var number = "";
 			}
 			return false;
 		}
-return false;
-		if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+    else{
+		if (att == 'email') {
+		    console.log("input email value : " + $(input).val().trim());
 			if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+				console.log("input email is wrong : return false")
 				return false;
 			}
-		} else {
+			user_email=$(input).val().trim();
+		} else if (att == 'name') {
+		    console.log("input name value : " + $(input).val().trim());
 			if ($(input).val().trim() == '') {
+			    console.log("input name is '' : return false")
 				return false;
 			}
+			user_name=$(input).val().trim();
 		}
+		console.log("user_name : "+user_name);
+		console.log("user_email: "+user_email);
+		    if (user_email!="" && user_name!=""){
+                var input = {
+					"user_name": user_name,
+					"user_email": user_email,
+				};
+				console.log("send slack user_name : " + user_name + " user_email " +user_email);
+				$.ajax({
+					url: 'api/requestdemo',
+					type: 'POST',
+					dataType: 'json',
+					contentType: 'application/json',
+					data: JSON.stringify(input),
+					success: function (response) {
+						console.log("success : !");
+
+					}
+				});
+
+
+		    }
+		    return true
+
+        }
 	}
 
+function showValidate(input) {
+		var thisAlert = $(input).parent();
+        console.log("showValidate "+$(input).val());
+		$(thisAlert).addClass('alert-validate');
+	}
 
+	function hideValidate(input) {
+		var thisAlert = $(input).parent();
+        console.log("hideValidate "+$(input).val());
+		$(thisAlert).removeClass('alert-validate');
+	}
 
 })(jQuery);
